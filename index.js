afe349535c7141d574c6bf179635d939901235c3
Hieutrinh1505 <60626013+Hieutrinh1505@users.noreply.github.com>
2020-04-19 22:47:59 -0500

    update

---
+++ index.js
@@ -0,0 +1,81 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var login = require('./server/test');
+
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.set('view engine', 'ejs'); 
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	
+	response.send("Hello world");
+});
+app.get('/home', function(req, res) {
+	res.sendfile(path.join(__dirname,'./views/patient_home.html'));
+});
+app.get('/nursehome', function(req, res) {
+	res.sendfile(path.join(__dirname,'./views/nurse_home.html'));
+});
+
+app.get('/auth',function(req,res) {
+	res.sendFile(path.join(__dirname,'./views/patient_login_signup.html'));
+});
+app.post('/auth', login.auth);
+app.get('/employeeauth',function(req,res){
+	res.sendFile(path.join(__dirname,'./views/employee_login.html'));
+app.get('/dhome',function(req,res){
+	res.sendFile(path.join(__dirname,'./views/doctor_home.html'));
+});
+
+});
+app.post('/employeeauth',login.employeeauth,function(req,res){
+
+});
+app.get('/logout', function(req, res){
+    req.session.destroy(function(){
+       console.log("user logged out.")
+    });
+    res.redirect('/auth');
+ });
+
+
+app.post('/register',login.register);
+
+
+app.get('/dappointment',login.dappointment);
+app.post('/dappointment',login.cancel);
+
+app.get('/employeeprescription',login.employeeprescription);
+app.post('/employeeprescription',login.submitprescription);
+
+app.get('/notes',login.notes);
+app.post('/notes',login.updatenotes);
+
+app.get('/appointment',login.appointment);
+app.post('/appointment',login.scheduleappointment);
+
+app.get('/history',login.history);
+app.get('/location',login.location);
+
+app.get('/patientprescription',login.patientprescription);
+app.post('/report1',login.report1);
+
+app.get('/report',function(req,res) {
+	res.render('reports');
+});
+
+
+app.listen(3000);
+
+
